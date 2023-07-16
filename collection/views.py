71772ffb18ec800dc40ec818255ec063d0a3366a
Rahul Narang <narang.rahul1298@gmail.com>
2023-07-17 02:09:46 +0530

    third-part-integration

--- collection/views.py
+++ collection/views.py
@@ -1,13 +1,20 @@
+from dotenv import load_dotenv
 import requests
 import os
-
-from django.conf import settings
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class MayaMovieApi(APIView):
     """Third party movie API integration."""
+    def __init__(self):
+        """Initialize maya movie API."""
+        load_dotenv('api_credentials.env')
+        self.url = os.environ.get('API_URL')
+        self.username = os.environ.get('API_USERNAME')
+        self.password = os.environ.get('API_PASSWORD')
+
     def get(self, request):
+        """Fetch movie list."""
         requests.get()
         
